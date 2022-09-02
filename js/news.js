f27678826032fbf1dc6809a055e3acf62dbf39ca
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-09-02 23:09:24 +0600

    Second Commit For Assignment8 & Nabar+Category Section Done

--- js/news.js
+++ js/news.js
@@ -1,1 +1,53 @@
-console.log("JS File Connected");
+/* Load All Categories API */
+const loadAllCatories = async () =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+
+    try{
+        const res = await fetch(url);
+        const categoryData = await res.json();
+        displayCategory(categoryData.data.news_category);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+
+/* Display All Category */
+const displayCategory = categories =>{
+    // console.log(categories);
+    const categorisUl = document.getElementById('categories-ul');
+
+    categories.forEach(category =>{
+        // console.log(category);
+        const categoryLi = document.createElement('li');
+
+        categoryLi.innerHTML = `<button onclick="categoryNewsShow('${category.category_id}')" type="button" class="btn category-btn">${category.category_name}</button>
+        `;
+        categorisUl.appendChild(categoryLi);
+    })
+}
+
+
+/* Show Categor Wise News */
+const categoryNewsShow = async (categoryId) =>{
+    // console.log(categoryId);
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+    try{
+        const res = await fetch(url);
+        const newsData = await res.json();
+        console.log(newsData.data);
+    }
+    catch(error){
+        console.log(error);
+    }
+
+}
+
+const displayNews = categoriesNewsData =>{
+    console.log(categoriesNewsData);
+}
+
+
+
+loadAllCatories();
